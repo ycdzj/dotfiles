c906bc8490b5408f8403b23318459078f8d5006f
ycdzj <yczhao0123@gmail.com>
2022-11-08 23:38:34 +0800

    feat(clean.sh): prompt before remove

--- clean.sh
+++ clean.sh
@@ -1,3 +1,46 @@
-rm -rf ~/.local/share/nvim
-rm -rf ~/.config/nvim
-rm -rf ~/.vim
+#!/bin/bash
+
+files=(
+    ~/.local/share/nvim
+    ~/.config/nvim
+
+    ~/.vim
+    ~/.vimrc
+
+    ~/.tmux.conf
+    ~/.zshrc
+    ~/.oh-my-zsh
+)
+
+files_to_remove=()
+for file in "${files[@]}"; do
+    if [ -e "$file" ]; then
+        files_to_remove+=("$file")
+    fi
+done
+
+if [ ${#files_to_remove[@]} -eq 0 ]; then
+    echo "No file needs to remove."
+    exit
+fi
+
+echo "Files to remove:"
+for file in "${files_to_remove[@]}"; do
+    echo "$file"
+done
+
+while true; do
+    read -p "Continue?[y/n] " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "removing..."
+mkdir ~/.trash
+for file in "${files_to_remove[@]}"; do
+    mv --backup=t "$file" ~/.trash
+done
+
