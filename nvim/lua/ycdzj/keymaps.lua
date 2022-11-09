bcdb2007a164ba078841ec9ea5b258728d07b11e
ycdzj <yczhao0123@gmail.com>
2022-11-09 17:32:44 +0800

    feat(tmux): update keybindings

--- nvim/lua/ycdzj/keymaps.lua
+++ nvim/lua/ycdzj/keymaps.lua
@@ -1,23 +1,19 @@
 -- set leader key to <SPACE>
 vim.g.mapleader = " "
 
--- use <C-c> as <ESC>
-vim.keymap.set("", "<C-c>", "<ESC>")
-
--- clear search highlights
-vim.keymap.set("n", "<leader>ch", ":nohl<CR>")
-
--- delete single character without yanking
+-- x to delete single character without yanking
 vim.keymap.set("n", "x", '"_x')
 
--- windows
-vim.keymap.set("n", "q", ":q<CR>")
+-- q to quit
+vim.keymap.set("n", "<leader>q", ":q<CR>")
 
--- tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-vim.keymap.set("n", "<leader>tq", ":tabclose<CR>") -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>") -- previouse tab
-
--- nvim-tree
+-- e to open nvim-tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeOpen<CR>")
+
+-- tmux
+vim.g.tmux_navigator_no_mappings = 1
+vim.keymap.set("n", "<C-Left>", ":<C-u>TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-Down>", ":<C-u>TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-Up>", ":<C-u>TmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-Right>", ":<C-u>TmuxNavigateRight<CR>")
+
