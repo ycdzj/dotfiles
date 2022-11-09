ca2b5f4d8784dbccc49e0a7982cd0197935c71a3
ycdzj <yczhao0123@gmail.com>
2022-11-09 13:07:15 +0800

    feat(tmux): add Vim Tmux Navigator

--- nvim/lua/ycdzj/keymaps.lua
+++ nvim/lua/ycdzj/keymaps.lua
@@ -13,13 +13,8 @@ vim.keymap.set("n", "x", '"_x')
 -- windows
 vim.keymap.set("n", "<leader>v", "<C-w>v")
 vim.keymap.set("n", "<leader>s", "<C-w>s")
-vim.keymap.set("n", "<leader>h", "<C-w>h")
-vim.keymap.set("n", "<leader>j", "<C-w>j")
-vim.keymap.set("n", "<leader>k", "<C-w>k")
-vim.keymap.set("n", "<leader>l", "<C-w>l")
 vim.keymap.set("n", "<leader>=", "<C-w>=") -- make split windows equal width & height
 vim.keymap.set("n", "q", ":q<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
 
 -- tabs
 vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
