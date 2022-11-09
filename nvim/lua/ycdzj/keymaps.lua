7be50f3d616c5b00a65f8e22f90138f9157670ea
ycdzj <yczhao0123@gmail.com>
2022-11-09 22:44:26 +0800

    feat(nvim): add telescope, gitsigns

--- nvim/lua/ycdzj/keymaps.lua
+++ nvim/lua/ycdzj/keymaps.lua
@@ -1,5 +1,5 @@
 -- set leader key to <SPACE>
-vim.g.mapleader = " "
+vim.g.mapleader = ";"
 
 -- x to delete single character without yanking
 vim.keymap.set("n", "x", '"_x')
@@ -17,3 +17,9 @@ vim.keymap.set("n", "<C-Down>", ":<C-u>TmuxNavigateDown<CR>")
 vim.keymap.set("n", "<C-Up>", ":<C-u>TmuxNavigateUp<CR>")
 vim.keymap.set("n", "<C-Right>", ":<C-u>TmuxNavigateRight<CR>")
 
+-- telescope
+local ts_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', ts_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', ts_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', ts_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', ts_builtin.help_tags, {})
