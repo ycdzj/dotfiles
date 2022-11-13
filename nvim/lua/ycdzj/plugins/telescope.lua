c3dfcf504b8a2121b9407030c61c302193b0e507
ycdzj <yczhao0123@gmail.com>
2022-11-13 20:27:02 +0800

    feat(telescope): use default keybindings

--- nvim/lua/ycdzj/plugins/telescope.lua
+++ nvim/lua/ycdzj/plugins/telescope.lua
@@ -1,13 +1,3 @@
 telescope = require("telescope")
-actions = require("telescope.actions")
-telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-j>"] = actions.move_selection_next,
-      },
-    },
-  },
-})
+telescope.setup()
 telescope.load_extension("fzf")
