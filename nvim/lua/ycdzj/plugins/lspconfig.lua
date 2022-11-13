b8f7cacdac50fdd996534f8419baddebcaef3304
ycdzj <yczhao0123@gmail.com>
2022-11-13 16:35:54 +0800

    Revert "feat(nvim): add nvim-ufo"
    This reverts commit b255dfcd8473f931e0039d6c7e7aa26751945cbd.

--- nvim/lua/ycdzj/plugins/lspconfig.lua
+++ nvim/lua/ycdzj/plugins/lspconfig.lua
@@ -1,3 +1,5 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true }
   vim.keymap.set("n", "ge", vim.diagnostic.open_float, opts)
@@ -14,12 +16,6 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "gf", vim.lsp.buf.format, bufopts)
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
--- needed by ufo
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
-}
 local lspconfig = require("lspconfig")
 lspconfig.clangd.setup({
   on_attach = on_attach,
