fa3891630ee2af0fe67cba488e085abbbbffd51f
ycdzj <yczhao0123@gmail.com>
2022-11-17 20:55:23 +0800

    feat(nvim): update config

--- nvim/lua/ycdzj/plugins/nvim-cmp.lua
+++ nvim/lua/ycdzj/plugins/nvim-cmp.lua
@@ -10,7 +10,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
